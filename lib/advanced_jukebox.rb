553e33eb1a666bee388c4af44d00f4f851a62f0f
Tracy Ngo <tngo1206@gmail.com>
2019-03-18 02:51:23 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -12,16 +12,26 @@
 # "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
+  
 def help
-  #this method should be the same as in jukebox.rb
+    #this method should be the same as in jukebox.rb
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
 
 end
+
 
 
 
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  my_songs.keys.each_with_index do |key, index|
+    puts "#{index+1}. #{key}"
+  end
 end
 
 
@@ -34,12 +44,45 @@ def play(my_songs)
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
   
-end
+  puts "Please enter a song name:"
+    user_response = gets.chomp
+    my_songs.each { |song, location|
+      if user_response == song
+        system 'open ' + location
+      end
+        }
+    if user_response == "list"
+      list(my_songs)
+      play(my_songs)
+    else
+      puts "Invalid input, please try again:"
+      play(my_songs)
+    end
+  end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+    help
+  input = ""
+  while input != "exit"
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when "play"
+      play(my_songs)
+    when "list"
+      list(my_songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox    
+    else
+      puts "Invalid input, please try again"
+    end
+  end
 end
